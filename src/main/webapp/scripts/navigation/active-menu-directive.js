8614fbf5e8d40744c62d5681c13cd0cf1915e17f
Romain Fromi <Romain.Fromi@nespresso.com>
2014-10-24 18:49:38 +0200

    Isolating Angular directives into distinct files

---
+++ src/main/webapp/scripts/navigation/active-menu-directive.js
@@ -0,0 +1,19 @@
+angular.module('TicTacToe').directive('activeMenu', function ($translate, $locale, tmhDynamicLocale) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var language = attrs.activeMenu;
+
+            scope.$watch(function () {
+                return $translate.use();
+            }, function (selectedLanguage) {
+                if (language === selectedLanguage) {
+                    tmhDynamicLocale.set(language);
+                    element.addClass('active');
+                } else {
+                    element.removeClass('active');
+                }
+            });
+        }
+    };
+});
